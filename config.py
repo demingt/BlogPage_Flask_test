07bc3ebcfa2c8cd4d775af870394a4acdd84b026
demingt <61338699+demingt@users.noreply.github.com>
2021-11-08 07:21:06 +0800

    updates

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess' #Flask-WTF extension uses SECRET_KEY to protect web forms against a nasty attack called Cross-Site Request Forgery or CSRF
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
